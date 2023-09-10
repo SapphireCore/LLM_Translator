21f00601d58c8363279e84729911464749c9d02e
Eugene Wen <SapphireCore@users.noreply.github.com>
2023-09-10 23:58:05 +0800

    Add files via upload

--- translator/translation_chain.py
+++ translator/translation_chain.py
@@ -15,7 +15,8 @@ class TranslationChain:
         # 翻译任务指令始终由 System 角色承担
         template = (
             """You are a translation expert, proficient in various languages. \n
-            Translates {source_language} to {target_language}."""
+            Translates {source_language} to {target_language}. \n
+            Please translate in {style} style."""
         )
         system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
@@ -33,13 +34,14 @@ class TranslationChain:
 
         self.chain = LLMChain(llm=chat, prompt=chat_prompt_template, verbose=verbose)
 
-    def run(self, text: str, source_language: str, target_language: str) -> (str, bool):
+    def run(self, text: str, source_language: str, target_language: str, style: str) -> (str, bool):
         result = ""
         try:
             result = self.chain.run({
                 "text": text,
                 "source_language": source_language,
                 "target_language": target_language,
+                "style": style,
             })
         except Exception as e:
             LOG.error(f"An error occurred during translation: {e}")
